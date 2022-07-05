73905de92fa3bf3d6dad398176a5a4d67a95290d
Fernando <fernando.ramirez>
2022-07-05 11:24:02 -0500

    CoreFramework

---
+++ src/main/java/config/Config.java
@@ -0,0 +1,32 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Config {
+    public WebDriver driver;
+    String browser = "chrome";
+
+    @BeforeSuite
+    public void beforTest(){
+        WebDriverManager.chromedriver().setup();
+        if(browser == "chrome"){
+            driver = new ChromeDriver();
+        } else {
+            driver = new FirefoxDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+    }
+    @AfterSuite
+    public void afterTest(){
+        driver.close();
+        //driver.quit();
+    }
+}
